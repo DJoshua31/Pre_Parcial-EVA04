ee134b719c3cc285042f51bf38b156a4241fec28
Diego Mejía <dmejiaguevara@gmail.com>
2025-11-18 18:11:40 -0600

    Creación de proyecto

---
+++ Preparcial_EVA04/src/test/java/com/utec/preparcial_security/PreparcialSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utec.preparcial_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparcialSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
